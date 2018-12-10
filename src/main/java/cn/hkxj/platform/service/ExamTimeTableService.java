e07d6b8f1bedfc894520e9e77162c49755fcbfdd
junrong.chen <jrchan02@gmail.com>
2018-12-10 10:23:30 +0800

    删除文件

--- src/main/java/cn/hkxj/platform/service/ExamTimeTableService.java
+++ src/main/java/cn/hkxj/platform/service/ExamTimeTableService.java
@@ -1,9 +1,7 @@
 package cn.hkxj.platform.service;
 
 import cn.hkxj.platform.exceptions.PasswordUncorrectException;
-import cn.hkxj.platform.mapper.ClassesMapper;
 import cn.hkxj.platform.mapper.ExamTimeTableMapper;
-import cn.hkxj.platform.pojo.Classes;
 import cn.hkxj.platform.pojo.ExamTimeTable;
 import cn.hkxj.platform.pojo.ExamTimeTableExample;
 import cn.hkxj.platform.pojo.Student;
@@ -13,11 +11,7 @@ import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * @author junrong.chen
@@ -28,29 +22,26 @@ import java.util.concurrent.Executors;
 public class ExamTimeTableService {
     @Resource
     private AppSpiderService appSpiderService;
-    @Resource
-    private ClassService classService;
+
     @Resource
     private ExamTimeTableMapper examTimeTableMapper;
 
 
     public List<ExamTimeTable> getExamTimeTableByStudent(Student student) {
-        Classes clazz = classService.getClassByStudent(student);
-        List<Integer> timeTableIdList = examTimeTableMapper.selectExamIdIdByClassId(clazz.getId());
+        Integer classId = student.getClasses().getId();
+        List<Integer> timeTableIdList = examTimeTableMapper.selectExamIdIdByClassId(classId);
         if(timeTableIdList.size() == 0){
-            ArrayList<ExamTimeTable> examList = null;
+            ArrayList<ExamTimeTable> examList;
             try {
                 examList = appSpiderService.getExamByAccount(student.getAccount());
             } catch (PasswordUncorrectException e) {
-                return new ArrayList<ExamTimeTable>();
+                return new ArrayList<>();
             }
             if(CollectionUtils.isEmpty(examList)){
                 return examList;
             }
 
-            ExecutorService executorService = Executors.newSingleThreadExecutor();
-            ArrayList<ExamTimeTable> finalExamList = examList;
-            executorService.execute(() -> saveExamTimeTask(clazz.getId(), finalExamList));
+            saveExamTimeTask(classId, examList);
             return examList;
         }
         else {
@@ -66,6 +57,7 @@ public class ExamTimeTableService {
      * @param classId
      * @param examTimeTableList
      */
+
     private void saveExamTimeTask(int classId, List<ExamTimeTable> examTimeTableList){
         log.info("task run");
         ArrayList<Integer> examIdList = new ArrayList<>();
