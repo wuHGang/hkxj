bd8583ea21665b6217c1150cf562709ec1258bc8
yuki <cxdexx@gmail.com>
2018-12-21 23:19:35 +0800

    对每日课程推送功能进行了修改

--- src/main/java/cn/hkxj/platform/PlatformApplication.java
+++ src/main/java/cn/hkxj/platform/PlatformApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.Resource;
 
 @EnableScheduling
 @SpringBootApplication
+@PropertySource(value = "application-prod.properties")
 public class PlatformApplication {
 
 	public static void main(String[] args) {
