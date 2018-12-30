abda629a5fc14f6456fc708f618d2848a3efac4d
XieDong <813281860@qq.com>
2018-12-30 12:01:48 +0800

    新增服务记录task类

---
+++ src/main/java/cn/hkxj/platform/pojo/Task.java
@@ -0,0 +1,57 @@
+package cn.hkxj.platform.pojo;
+
+import java.util.Date;
+
+public class Task {
+    private Integer id;
+
+    private String openid;
+
+    private Integer updateType;
+
+    private Integer count;
+
+    private Date gmtModified;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public Integer getUpdateType() {
+        return updateType;
+    }
+
+    public void setUpdateType(Integer updateType) {
+        this.updateType = updateType;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+
+}
