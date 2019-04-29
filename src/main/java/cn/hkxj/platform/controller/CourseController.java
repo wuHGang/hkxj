577c0bda01a8dbec5ddf0c8a6e380908d745e790
zhouqinglai <zhouqinglai@baijiahulian.com>
2019-04-29 13:25:22 +0800

    empty commit

--- src/main/java/cn/hkxj/platform/controller/CourseController.java
+++ src/main/java/cn/hkxj/platform/controller/CourseController.java
@@ -6,6 +6,7 @@ import cn.hkxj.platform.pojo.WebResponse;
 import cn.hkxj.platform.service.CourseService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
