3c7a23259262bd4ec302e629565e2fe691755b48
JR Chan <jrchan02@gmail.com>
2019-05-03 12:10:09 +0800

    remove

--- src/main/java/cn/hkxj/platform/service/wechat/handler/messageHandler/ExampleTemplateHandler.java
+++ src/main/java/cn/hkxj/platform/service/wechat/handler/messageHandler/ExampleTemplateHandler.java
@@ -1,9 +1,10 @@
 package cn.hkxj.platform.service.wechat.handler.messageHandler;
 
 import cn.hkxj.platform.builder.TemplateBuilder;
-import cn.hkxj.platform.service.wechat.handler.AbstractHandler;
+import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
+import me.chanjar.weixin.mp.api.WxMpMessageHandler;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
@@ -15,38 +16,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Component
-public class ExampleTemplateHandler extends AbstractHandler {
+public class ExampleTemplateHandler implements WxMpMessageHandler {
 
-	@Autowired
-	private TemplateBuilder templateBuilder;
+    @Autowired
+    private TemplateBuilder templateBuilder;
 
-	@Override
-	public WxMpXmlOutMessage handle(WxMpXmlMessage wxMpXmlMessage, Map<String, Object> map, WxMpService wxMpService, WxSessionManager wxSessionManager) throws WxErrorException {
+    @Override
+    public WxMpXmlOutMessage handle(WxMpXmlMessage wxMpXmlMessage, Map<String, Object> map, WxMpService wxMpService, WxSessionManager wxSessionManager) throws WxErrorException {
 
-		System.out.println("！！！模板测试！！！");
-		try {
-			List<WxMpTemplateData> datas = new ArrayList<WxMpTemplateData>();
+        System.out.println("！！！模板测试！！！");
+        try {
+            List<WxMpTemplateData> datas = new ArrayList<WxMpTemplateData>();
 
-			WxMpTemplateData first = new WxMpTemplateData();
-			first.setName("first");
-			first.setValue("绑定页面");
-			WxMpTemplateData name = new WxMpTemplateData();
-			name.setName("name");
-			name.setValue("测试");
-			WxMpTemplateData grade = new WxMpTemplateData();
-			grade.setName("grade");
-			grade.setValue("样例");
-			datas.add(first);
-			datas.add(name);
-			datas.add(grade);
-			String url = "http://suagr.tunnel.echomod.cn/login/" + wxMpXmlMessage.getFromUser();
-			wxMpService.getTemplateMsgService().sendTemplateMsg(templateBuilder.build(wxMpXmlMessage, datas, url));
-			return null;
-		} catch (Exception e) {
-			this.logger.error("在组装返回信息时出现错误 {}", e.getMessage());
-		}
-		return null;
-	}
+            WxMpTemplateData first = new WxMpTemplateData();
+            first.setName("first");
+            first.setValue("绑定页面");
+            WxMpTemplateData name = new WxMpTemplateData();
+            name.setName("name");
+            name.setValue("测试");
+            WxMpTemplateData grade = new WxMpTemplateData();
+            grade.setName("grade");
+            grade.setValue("样例");
+            datas.add(first);
+            datas.add(name);
+            datas.add(grade);
+            String url = "http://suagr.tunnel.echomod.cn/login/" + wxMpXmlMessage.getFromUser();
+            wxMpService.getTemplateMsgService().sendTemplateMsg(templateBuilder.build(wxMpXmlMessage, datas, url));
+            return null;
+        } catch (Exception e) {
+            log.error("在组装返回信息时出现错误 {}", e.getMessage());
+        }
+        return null;
+    }
 
 }
