73371d99b74c9bd82599f793c29b29d110b99b9e
付明才 <78781170@qq.com>
2019-06-01 21:55:51 +0800

    枚举值映射文字

--- src/main/java/cn/hkxj/platform/pojo/constant/SubjectFirstProperty.java
+++ src/main/java/cn/hkxj/platform/pojo/constant/SubjectFirstProperty.java
@@ -29,16 +29,41 @@ public enum  SubjectFirstProperty {
     }
 
 
-    public static SubjectFirstProperty getPropertyByChinese(String chinese){
-        switch (chinese){
+    public static SubjectFirstProperty getPropertyByChinese(String chinese) {
+        switch (chinese) {
             case "工学":
                 return ENGINEERING;
             case "理学":
                 return SCIENCE;
-            case"管理学":
+            case "管理学":
                 return MANAGEMENT;
+            case "法学":
+                return LAW;
+            case "经济学":
+                return ECONOMICS;
+            case "文学":
+                return LITERATURE;
             default:
                 throw new IllegalArgumentException(chinese);
         }
     }
+
+    public static String getPropertyByPropertyFirst(String propertyFirst){
+        switch (propertyFirst) {
+            case "ENGINEERING":
+                return "工学";
+            case "SCIENCE":
+                return "理学";
+            case "MANAGEMENT":
+                return "管理学";
+            case "LAW":
+                return "法学";
+            case "ECONOMICS":
+                return "经济学";
+            case "LITERATURE":
+                return "文学";
+            default:
+                throw new IllegalArgumentException(propertyFirst);
+            }
+        }
 }
