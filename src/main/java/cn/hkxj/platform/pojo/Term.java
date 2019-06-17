0c30daad813390fb5e91c0abc09d2ba7342681b4
yuki <cxdexx@gmail.com>
2019-06-17 23:18:48 +0800

    重写了Term的equals和hashcode方法

--- src/main/java/cn/hkxj/platform/pojo/Term.java
+++ src/main/java/cn/hkxj/platform/pojo/Term.java
@@ -1,6 +1,8 @@
 package cn.hkxj.platform.pojo;
 
 import com.google.common.base.MoreObjects;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 /**
  * @author JR Chan
@@ -51,6 +53,29 @@ public class Term {
         this.order = order;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Term term = (Term) o;
+
+        return new EqualsBuilder()
+                .append(startYear, term.startYear)
+                .append(endYear, term.endYear)
+                .append(order, term.order)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(startYear)
+                .append(endYear)
+                .append(order)
+                .toHashCode();
+    }
 
     @Override
     public String toString() {
